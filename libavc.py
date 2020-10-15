b6b6dcc84ecb252cb202510045e1b0f3ab3d276e
Jonas Ampferl <jonas@ampferl.com>
2020-10-15 09:50:07 +0200

    Added object_read method

--- libavc.py
+++ libavc.py
@@ -67,13 +67,39 @@ class GitObject(object):
         if data is not None:
             self.deserialize(data)
 
-
     def serialize(self):
         raise Exception("Unimplemented!")
 
-
     def deserialize(self, data):
         raise Exception("Unimplemented!")
+
+
+def object_read(repo, sha):
+    path = repo_file(repo, "objects", sha[0:2], sha[2:0])
+
+    with open(path, "rb") as f:
+        raw = zlib.decompress(f.read())
+
+        x = raw.find(b' ')
+        fmt = raw[0:x]
+
+        y = raw.find(b'\x00', x)
+        size = int(raw[x:y].decode("ascii"))
+        if size != len(raw)-y-1:
+            raise Exception("Malformed object {0}: bad length".format(sha))
+
+        if fmt == b'commit':
+            c = GitCommit
+        elif fmt == b'tree':
+            c = GitTree
+        elif fmt == b'tag':
+            c = GitTag
+        elif fmt == b'blob':
+            c = GitBlob
+        else:
+            raise Exception("Unknown type %s for object %s".format(fmt.decode("ascii"), sha))
+
+        return c(repo, raw[y+1:])
 
 
 def repo_path(repo, *path):
