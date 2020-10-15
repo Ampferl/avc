456d5f116ef76dbb16a3afc31bb00067aa21a72d
Jonas Ampferl <jonas@ampferl.com>
2020-10-15 09:12:53 +0200

    Added repo_default_config method

--- libavc.py
+++ libavc.py
@@ -113,3 +113,16 @@ def repo_create(path):
         config.write(f)
 
     return repo
+
+
+def repo_default_config():
+    ret = configparser.ConfigParser()
+
+    ret.add_section("core")
+    ret.set("core", "repositoryformatversion", "0")
+    ret.set("core", "filemode", "false")
+    ret.set("core", "bare", "false")
+
+    return ret
+
+
