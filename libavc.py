5bc73230f1c8c46527f4d7a4ea3fdbf5e4369c69
Jonas Ampferl <jonas@ampferl.com>
2020-10-15 13:14:55 +0200

    Added new main statement with all parsers and subparsers

--- libavc.py
+++ libavc.py
@@ -12,38 +12,100 @@ import time
 import urllib.request
 import zlib
 
-argparser = argparse.ArgumentParser(description="Another Version Control")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    sub_parsers = parser.add_subparsers(dest='command', metavar='command')
+    sub_parsers.required = True
 
-argsubparser = argparser.add_subparsers(title="Commands", dest="command")
-argsubparser.required = True
+    sub_parser = sub_parsers.add_parser('add',
+            help='add file(s) to index')
+    sub_parser.add_argument('paths', nargs='+', metavar='path',
+            help='path(s) of files to add')
 
+    sub_parser = sub_parsers.add_parser('cat-file',
+            help='display contents of object')
+    valid_modes = ['commit', 'tree', 'blob', 'size', 'type', 'pretty']
+    sub_parser.add_argument('mode', choices=valid_modes,
+            help='object type (commit, tree, blob) or display mode (size, '
+                 'type, pretty)')
+    sub_parser.add_argument('hash_prefix',
+            help='SHA-1 hash (or hash prefix) of object to display')
 
-def main(argv=sys.argv[1:]):
-    args = argparser.parse_args(argv)
+    sub_parser = sub_parsers.add_parser('commit',
+            help='commit current state of index to master branch')
+    sub_parser.add_argument('-a', '--author',
+            help='commit author in format "A U Thor <author@example.com>" '
+                 '(uses GIT_AUTHOR_NAME and GIT_AUTHOR_EMAIL environment '
+                 'variables by default)')
+    sub_parser.add_argument('-m', '--message', required=True,
+            help='text of commit message')
 
-    if args.command == "init":
-        cmd_init(args)
-    elif args.command == "cat-file":
-        cmd_cat_file(args)
-    elif args.command == "hash-object":
-        cmd_hash_object(args)
-    elif args.command == "log":
-        cmd_log(args)
-    elif args.command == "ls-tree":
-        cmd_ls_tree(args)
-    elif args.command == "checkout":
-        cmd_checkout(args)
-    elif args.command == "show-ref":
-        cmd_show_ref(args)
-    elif args.command == "tag":
-        cmd_tag(args)
-    elif args.command == "rev-parse":
-        cmd_rev_parse(args)
-    # elif args.command == "add"         : cmd_add(args)
-    # elif args.command == "commit"      : cmd_commit(args)
-    # elif args.command == "merge"       : cmd_merge(args)
-    # elif args.command == "rebase"      : cmd_rebase(args)
-    # elif args.command == "rm"          : cmd_rm(args)
+    sub_parser = sub_parsers.add_parser('diff',
+            help='show diff of files changed (between index and working '
+                 'copy)')
+
+    sub_parser = sub_parsers.add_parser('hash-object',
+            help='hash contents of given path (and optionally write to '
+                 'object store)')
+    sub_parser.add_argument('path',
+            help='path of file to hash')
+    sub_parser.add_argument('-t', choices=['commit', 'tree', 'blob'],
+            default='blob', dest='type',
+            help='type of object (default %(default)r)')
+    sub_parser.add_argument('-w', action='store_true', dest='write',
+            help='write object to object store (as well as printing hash)')
+
+    sub_parser = sub_parsers.add_parser('init',
+            help='initialize a new repo')
+    sub_parser.add_argument('repo',
+            help='directory name for new repo')
+
+    sub_parser = sub_parsers.add_parser('ls-files',
+            help='list files in index')
+    sub_parser.add_argument('-s', '--stage', action='store_true',
+            help='show object details (mode, hash, and stage number) in '
+                 'addition to path')
+
+    sub_parser = sub_parsers.add_parser('push',
+            help='push master branch to given git server URL')
+    sub_parser.add_argument('git_url',
+            help='URL of git repo, eg: https://github.com/benhoyt/pygit.git')
+    sub_parser.add_argument('-p', '--password',
+            help='password to use for authentication (uses GIT_PASSWORD '
+                 'environment variable by default)')
+    sub_parser.add_argument('-u', '--username',
+            help='username to use for authentication (uses GIT_USERNAME '
+                 'environment variable by default)')
+
+    sub_parser = sub_parsers.add_parser('status',
+            help='show status of working copy')
+
+    args = parser.parse_args()
+    if args.command == 'add':
+        add(args.paths)
+    elif args.command == 'cat-file':
+        try:
+            cat_file(args.mode, args.hash_prefix)
+        except ValueError as error:
+            print(error, file=sys.stderr)
+            sys.exit(1)
+    elif args.command == 'commit':
+        commit(args.message, author=args.author)
+    elif args.command == 'diff':
+        diff()
+    elif args.command == 'hash-object':
+        sha1 = hash_object(read_file(args.path), args.type, write=args.write)
+        print(sha1)
+    elif args.command == 'init':
+        init(args.repo)
+    elif args.command == 'ls-files':
+        ls_files(details=args.stage)
+    elif args.command == 'push':
+        push(args.git_url, username=args.username, password=args.password)
+    elif args.command == 'status':
+        status()
+    else:
+        assert False, 'unexpected command {!r}'.format(args.command)
 
 
 class GitRepository(object):
