a86099502cacd24820844de1afa8d6a0cde6fe25
Jonas Ampferl <jonas@ampferl.com>
2020-10-15 08:47:28 +0200

    Added main function with args i want to add to avc

--- libavc.py
+++ libavc.py
@@ -11,3 +11,22 @@ argparser = argparse.ArgumentParser(description="Another Version Control")
 
 argsubparser = argparser.add_subparsers(title="Commands", dest="command")
 argsubparser.required = True
+
+
+def main(argv=sys.argv[1:]):
+    args = argparser.parse_args(argv)
+
+    # if   args.command == "add"         : cmd_add(args)
+    # elif args.command == "cat-file"    : cmd_cat_file(args)
+    # elif args.command == "checkout"    : cmd_checkout(args)
+    # elif args.command == "commit"      : cmd_commit(args)
+    # elif args.command == "hash-object" : cmd_hash_object(args)
+    # elif args.command == "init"        : cmd_init(args)
+    # elif args.command == "log"         : cmd_log(args)
+    # elif args.command == "ls-tree"     : cmd_ls_tree(args)
+    # elif args.command == "merge"       : cmd_merge(args)
+    # elif args.command == "rebase"      : cmd_rebase(args)
+    # elif args.command == "rev-parse"   : cmd_rev_parse(args)
+    # elif args.command == "rm"          : cmd_rm(args)
+    # elif args.command == "show-ref"    : cmd_show_ref(args)
+    # elif args.command == "tag"         : cmd_tag(args)
