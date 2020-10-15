00e528139648654e3c18d91b3beef866f56cb750
Jonas Ampferl <jonas@ampferl.com>
2020-10-15 13:29:52 +0200

    implemented ls_files command

--- libavc.py
+++ libavc.py
@@ -93,6 +93,16 @@ def status():
             print('   ', path)
 
 
+def ls_files(details=False):
+    for entry in read_index():
+        if details:
+            stage = (entry.flags >> 12) & 3
+            print('{:6o} {} {:}\t{}'.format(
+                    entry.mode, entry.sha1.hex(), stage, entry.path))
+        else:
+            print(entry.path)
+
+
 def diff():
     changed, _, _ = get_status()
     entries_by_path = {e.path: e for e in read_index()}
