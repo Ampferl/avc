4eb26e7bc0389775e41971fd514f7fd44a77f076
Jonas Ampferl <jonas@ampferl.com>
2020-10-15 08:44:27 +0200

    Added argparser to require actually one argument

--- libavc.py
+++ libavc.py
@@ -6,3 +6,8 @@ import os
 import re
 import sys
 import zlib
+
+argparser = argparse.ArgumentParser(description="Another Version Control")
+
+argsubparser = argparser.add_subparsers(title="Commands", dest="command")
+argsubparser.required = True
