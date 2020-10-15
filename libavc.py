d0b2dcc0793c56a2b51163d13c42ffbe92a6416d
Jonas Ampferl <jonas@ampferl.com>
2020-10-15 09:39:13 +0200

    Added GitObject class (unimplemented)

--- libavc.py
+++ libavc.py
@@ -58,6 +58,24 @@ class GitRepository(object):
                 raise Exception("Unsupported repositoryformatversion %s" % vers)
 
 
+class GitObject(object):
+    repo = None
+
+    def __init__(self, repo, data=None):
+        self.repo = repo
+
+        if data is not None:
+            self.deserialize(data)
+
+
+    def serialize(self):
+        raise Exception("Unimplemented!")
+
+
+    def deserialize(self, data):
+        raise Exception("Unimplemented!")
+
+
 def repo_path(repo, *path):
     return os.path.join(repo.gitdir, *path)
 
